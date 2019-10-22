50f008219dedbd60b25acabeda876e2ac5e1efff
XavierCLL <xavier.corredor.llano@gmail.com>
2019-10-22 10:36:52 -0700

    Init code

---
+++ __init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ CCD Plugin
+                                 A QGIS plugin
+ Continuous Change Detection Plugin
+                              -------------------
+        copyright            : (C) 2019 by Xavier Corredor Llano, SMByC
+        email                : xcorredorl@ideam.gov.co
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load CCD_Plugin class from file CCD_Plugin.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .CCD_Plugin import CCD_Plugin
+    return CCD_Plugin(iface)
