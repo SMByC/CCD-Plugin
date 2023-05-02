69ed928265af0b32f97667996d11e8cda6249fa9
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2023-05-01 19:19:15 -0700

    Fix reload and install the update

--- __init__.py
+++ __init__.py
@@ -19,6 +19,8 @@
  ***************************************************************************/
  This script initializes the plugin, making it known to QGIS.
 """
+import ctypes
+import importlib
 import inspect
 import os
 import platform
@@ -41,15 +43,36 @@ def get_extlib_path():
     return os.path.abspath(os.path.join(os.path.dirname(__file__), extlib_path))
 
 
+def unload_all_dlls(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".dll"):
+                dll_path = os.path.join(root, file)
+                try:
+                    # Load the DLL
+                    dll = ctypes.windll.LoadLibrary(dll_path)
+                    # Unload the DLL
+                    ctypes.windll.kernel32.FreeLibrary(dll._handle)
+                except Exception as e:
+                    print(f"Error removing {file}: {e}")
+
+
 def check_dependencies():
     try:
         import ccd
+        importlib.reload(ccd)
 
         # update the ccd library if using an old version
         if len(inspect.signature(ccd.detect).parameters) != 18:
             extra_libs_path = get_extlib_path()
             # remove the extra libs ignoring the errors
+            if platform.system() == "Windows":
+                unload_all_dlls(extra_libs_path)
             shutil.rmtree(extra_libs_path, ignore_errors=True)
+            # show a message to the user to restart QGIS
+            msg = "The ccd library requires restarting QGIS to load the update after installing it."
+            QMessageBox.information(None, 'CCD-Plugin: Restart QGIS', msg, QMessageBox.Ok)
+
             return False
 
         return True
